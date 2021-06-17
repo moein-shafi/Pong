d2514bd09623188f547f37249ff703ab6d45cacc
Mohammad Salamati <salamati9819@gmail.com>
2021-06-17 18:54:52 +0430

    Bluetooth completed with variation of bugs

--- app/src/main/java/com/example/pong/ClientActivity.java
+++ app/src/main/java/com/example/pong/ClientActivity.java
@@ -1,19 +1,79 @@
 package com.example.pong;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 import android.widget.Toast;
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 public class ClientActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static String UUID_String = "PONG SERVER";
+    private static final UUID MY_UUID = UUID.nameUUIDFromBytes(UUID_String.getBytes());
+    private static final String TAG = "pongClient";
+
+    private class ConnectThread extends Thread {
+        private BluetoothAdapter bluetoothAdapter;
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            Toast.makeText(getApplicationContext(), "Connected to server", Toast.LENGTH_LONG).show();
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
 
     private static final int DISCOVER_DURATION = 300;
     private static final int REQUEST_BLU = 1;
@@ -24,14 +84,58 @@ public class ClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
 
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if (btAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show();
-        } else {
-            enableBluetooth();
+        enableBluetooth();
+
+        if (bluetoothAdapter == null) {
+            Log.e(TAG, "Device doesn't support bluetooth");
         }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(discoveryFinishReceiver, filter);
+
+//        if (pairedDevices.size() > 0) {
+//            // There are paired devices. Get the name and address of each paired device.
+//            for (BluetoothDevice device : pairedDevices) {
+//                String deviceName = device.getName();
+//                String deviceHardwareAddress = device.getAddress(); // MAC address
+//            }
+//        }
+
+        new ConnectThread(pairedDevices.iterator().next()).run();//start? //TODO CHECK ITERATOR.NEXT
     }
+
+    private final BroadcastReceiver discoveryFinishReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    discoveredDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                if (discoveredDevicesAdapter.getCount() == 0) {
+                    discoveredDevicesAdapter.add(getString(R.string.none_found));
+                }
+            }
+        }
+    };
 
     public void enableBluetooth(){
         Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
@@ -41,6 +145,11 @@ public class ClientActivity extends AppCompatActivity {
         startActivityForResult(discoveryIntent, REQUEST_BLU);
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
     }
+
 }
--- app/src/main/java/com/example/pong/ServerActivity.java
+++ app/src/main/java/com/example/pong/ServerActivity.java
@@ -1,24 +1,75 @@
 package com.example.pong;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.os.Environment;
-import android.view.View;
+import android.util.Log;
 import android.widget.Toast;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.UUID;
+
 
 public class ServerActivity extends AppCompatActivity {
+    private static String UUID_String = "PONG SERVER";
+    private static final UUID MY_UUID = UUID.nameUUIDFromBytes(UUID_String.getBytes());
+    private static final String NAME = "pong";
+    private static final String TAG = "pongServer";
+    private static final int REQUEST_ENABLE_BT = 1;
+    private final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+        public AcceptThread() {
+
+            // Use a temporary object that is later assigned to mmServerSocket
+            // because mmServerSocket is final.
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code.
+                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's listen() method failed", e);
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned.
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    Log.e(TAG, "Socket's accept() method failed", e);
+                    break;
+                }
+
+                if (socket != null) {
+                    // A connection was accepted. Perform work associated with
+                    // the connection in a separate thread.
+                    Toast.makeText(getApplicationContext(), "One device connected", Toast.LENGTH_LONG).show();
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the connect socket", e);
+            }
+        }
+    }
 
     private static final int DISCOVER_DURATION = 300;
     private static final int REQUEST_BLU = 1;
@@ -28,59 +79,65 @@ public class ServerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
 
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(btAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show();
-        } else {
-            enableBluetooth();
+        if (bluetoothAdapter == null) {
+            Log.e(TAG, "Device doesn't support bluetooth");
         }
-    }
 
-    public void enableBluetooth() {
-        Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-
-        Intent intent = discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
-
-        startActivityForResult(discoveryIntent, REQUEST_BLU);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if(resultCode == DISCOVER_DURATION && requestCode == REQUEST_BLU) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_STREAM, "YO BROOOOO");
-
-            PackageManager pm = getPackageManager();
-            List<ResolveInfo> appsList = pm.queryIntentActivities(intent, 0);
-
-            if(appsList.size() > 0) {
-                String packageName = null;
-                String className = null;
-                boolean found = false;
-
-                for(ResolveInfo info : appsList) {
-                    packageName = info.activityInfo.packageName;
-                    if(packageName.equals("com.android.bluetooth")) {
-                        className = info.activityInfo.name;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if(!found) {
-                    Toast.makeText(this, "Bluetooth not found",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    intent.setClassName(packageName, className);
-                    startActivity(intent);
-                }
-            }
-        } else {
-            Toast.makeText(this, "Bluetooth is cancelled", Toast.LENGTH_LONG).show();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+
+        AcceptThread acceptThread = new AcceptThread();
+        acceptThread.run();
+
+
+//        enableBluetooth();
+
     }
+//
+//    public void enableBluetooth() {
+//        Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//
+//        startActivityForResult(discoveryIntent, REQUEST_BLU);
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        if(resultCode == DISCOVER_DURATION && requestCode == REQUEST_BLU) {
+//            Intent intent = new Intent();
+//            intent.setAction(Intent.ACTION_SEND);
+//            intent.setType("text/plain");
+//            intent.putExtra(Intent.EXTRA_STREAM, "YO BROOOOO");
+//
+//            PackageManager pm = getPackageManager();
+//            List<ResolveInfo> appsList = pm.queryIntentActivities(intent, 0);
+//
+//            if(appsList.size() > 0) {
+//                String packageName = null;
+//                String className = null;
+//                boolean found = false;
+//
+//                for(ResolveInfo info : appsList) {
+//                    packageName = info.activityInfo.packageName;
+//                    if(packageName.equals("com.android.bluetooth")) {
+//                        className = info.activityInfo.name;
+//                        found = true;
+//                        break;
+//                    }
+//                }
+//
+//                if(!found) {
+//                    Toast.makeText(this, "Bluetooth not found",
+//                            Toast.LENGTH_LONG).show();
+//                } else {
+//                    intent.setClassName(packageName, className);
+//                    startActivity(intent);
+//                }
+//            }
+//        } else {
+//            Toast.makeText(this, "Bluetooth is cancelled", Toast.LENGTH_LONG).show();
+//        }
+//    }
 }
